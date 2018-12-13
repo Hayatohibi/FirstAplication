f2b62e666eb57d4ceb9890d4e089758a251dec75
hayato.H <catshayato@gmail.com>
2018-12-13 19:19:57 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/hayato/hibi/firstaplication/MainActivity.java
@@ -0,0 +1,28 @@
+package jp.techacademy.hayato.hibi.firstaplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+    }
+}
